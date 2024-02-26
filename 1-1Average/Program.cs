d6f7ea92ed78f4dda7e8d2ae7148a92418a91664
Faisal Karim <Faisalmohammed2002@gmail.com>
2024-02-26 14:37:33 +1100

    Add project files.

---
+++ 1-1Average/Program.cs
@@ -0,0 +1,27 @@
+﻿float GetAverage(float[] inputs)
+{
+    float sum = 0;
+    foreach (var input in inputs)
+    {
+        sum += input;
+    }
+    return sum / inputs.Length;
+}
+
+
+float[] inputs = new float[] { -21, -22, -3, -4, 5, 6 };
+float average = GetAverage(inputs);
+Console.WriteLine(average);
+
+if (average >= 10)
+{
+    Console.WriteLine("Double Digits");
+}
+else
+{
+    Console.WriteLine("Single Digits");
+}
+if (average < 0)
+{
+    Console.WriteLine("Average value is in the negative");
+}
---
+++ 1-2Message/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace _1_2Message
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var myMessage = new Message("Hello, World! Greetings from Message Object.");
+            myMessage.Print();
+
+            string name = Console.ReadLine()!.ToLower();
+            string[] knownNames = new string[] { "faisal", "walima", "mathew", "james", "peter"};
+            
+            
+            var faisalMsg = new Message("Hi Faisal, how are you?");
+            var walimalMsg = new Message("Hi Walima, how are you?");
+            var mathewMsg = new Message("Hi Mathew, how are you?");
+            var jamesMsg = new Message("Hi James, how are you?");
+            var peterMsg = new Message("Hi Peter, how are you?");
+            var defaultMsg = new Message("Welcome, nice to me you.");
+
+            if (name == "faisal")
+            {
+                faisalMsg.Print();
+            }else if(name == "Walima")
+            {
+                walimalMsg.Print();
+            }else if (name == "mathew")
+            {
+                mathewMsg.Print(); 
+            }else if (name == "james")
+            {
+                jamesMsg.Print();
+            }else if (name == "peter")
+            {
+                peterMsg.Print();
+            }else
+            {
+                defaultMsg.Print();
+            }
+
+
+        }
+    }
+}
