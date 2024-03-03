2a473c40e31090ece00f719ae31f4feea67dd3e7
Faisal Karim <Faisalmohammed2002@gmail.com>
2024-03-03 12:10:04 +1100

    Iteration 1 Complete

---
+++ SwinAdventureCaseStudy/IdentifiableObject.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventureCaseStudy
+{
+    public class IdentifiableObject
+    {
+        List<string> _identifiers = new List<string>();
+
+        public string FirstId
+        {
+            get
+            {
+                if (_identifiers.Count == 0)
+                    return "";
+                {
+                    return _identifiers.First();
+                }
+            }
+        }
+
+        public IdentifiableObject(IEnumerable<string> idents)
+        {
+
+            IEnumerable<string> lowercaseIdents = idents.Select(ident => ident.ToLower());
+            _identifiers.AddRange(lowercaseIdents);
+        }
+
+        public bool AreYou(string id)
+        {
+            return _identifiers.Contains(id.ToLower());
+        }
+
+        public void AddIdentifier(string id)
+        {
+            _identifiers.Add(id.ToLower());
+        }
+
+    }
+}
---
+++ SwinAdventureUnitTests/IdentifiableObjectTests.cs
@@ -0,0 +1,66 @@
+using SwinAdventureCaseStudy;
+
+namespace SwinAdventureUnitTests
+{
+    public class IdentifiableObjectTests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [TestCase("fred")]
+        [TestCase("bob")]
+        public void TestAreYou(string testId)
+        {
+            IdentifiableObject identifiableObject = new IdentifiableObject(["fred", "bob"]);
+
+            Assert.IsTrue(identifiableObject.AreYou(testId));
+        }
+
+        [TestCase("id1")]
+        [TestCase("")]
+        [TestCase("id3")]
+        public void TestNotAreYou(string testId)
+        {
+            IdentifiableObject identifiableObject = new IdentifiableObject(["Fred", "Bob"]);
+
+            Assert.IsFalse(identifiableObject.AreYou(testId));
+        }
+
+        [TestCase("Fred")]
+        [TestCase("fred")]
+        [TestCase("freD")]
+        [TestCase("Bob")]
+        [TestCase("bob")]
+        public void TestCaseSensitiveAreYou(string testId)
+        {
+            IdentifiableObject identifiableObject = new IdentifiableObject(["Fred", "Bob"]);
+
+            Assert.IsTrue(identifiableObject.AreYou(testId));
+        }
+
+        [Test]
+        public void TestFirstId()
+        {
+            IdentifiableObject identifiableObject = new IdentifiableObject(["Fred", "Bob"]);
+            Assert.That(identifiableObject.FirstId, Is.EqualTo("fred"));
+        }
+        [Test]
+        public void TestFirstIdWithNoIds()
+        {
+            IdentifiableObject identifiableObject = new IdentifiableObject([]);
+            Assert.That(identifiableObject.FirstId, Is.EqualTo(""));
+        }
+
+        [TestCase("James")]
+        [TestCase("wiLL")]
+        public void TestAddId(string id)
+        {
+            IdentifiableObject identifiableObject = new IdentifiableObject(["Fred", "Bob"]);
+            identifiableObject.AddIdentifier(id);
+
+            Assert.IsTrue(identifiableObject.AreYou(id));
+        }
+    }
+}
