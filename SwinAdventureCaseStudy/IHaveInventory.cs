a46666d57db99c08e199d9627b9f5dd0bf0ee309
Faisal Karim <Faisalmohammed2002@gmail.com>
2024-03-21 18:33:52 +1100

    Iteration 4 Completed

---
+++ SwinAdventureCaseStudy/IHaveInventory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwinAdventureCaseStudy
+{
+    public interface IHaveInventory
+    {
+        public GameObject? Locate(string id);
+        public string Name { get; }
+
+        public Inventory Inventory { get; }
+    }
+}
